2d4359962ea0c51ccf5f5c985ca4795f0db67d80
Tina Fish <tina.nc.fish@gmail.com>
2016-11-04 21:53:10 +0000

    Type hint return values

--- src/IcsGenerator.php
+++ src/IcsGenerator.php
@@ -17,7 +17,7 @@ class IcsGenerator
      * @param mixed $url
      * @return $this
      */
-    public function setUrl($url)
+    public function setUrl($url): IcsGenerator
     {
         $this->url = $url;
 
@@ -28,7 +28,7 @@ class IcsGenerator
      * @param mixed $dtstart
      * @return $this
      */
-    public function setDtstart(DateTime $dtstart)
+    public function setDtstart(DateTime $dtstart): IcsGenerator
     {
         $this->dtstart = $dtstart;
 
@@ -39,7 +39,7 @@ class IcsGenerator
      * @param mixed $dtend
      * @return $this
      */
-    public function setDtend(DateTime $dtend)
+    public function setDtend(DateTime $dtend): IcsGenerator
     {
         $this->dtend = $dtend;
 
@@ -50,7 +50,7 @@ class IcsGenerator
      * @param mixed $summary
      * @return $this
      */
-    public function setSummary($summary)
+    public function setSummary($summary): IcsGenerator
     {
         $this->summary = $summary;
 
@@ -61,7 +61,7 @@ class IcsGenerator
      * @param mixed $description
      * @return $this
      */
-    public function setDescription($description)
+    public function setDescription($description): IcsGenerator
     {
         $this->description = $description;
 
@@ -72,14 +72,14 @@ class IcsGenerator
      * @param mixed $location
      * @return $this
      */
-    public function setLocation($location)
+    public function setLocation($location): IcsGenerator
     {
         $this->location = $location;
 
         return $this;
     }
 
-    public function getString()
+    public function getString(): string
     {
       $string = "BEGIN:VCALENDAR" . PHP_EOL;
       $string .= "VERSION:2.0" . PHP_EOL;
