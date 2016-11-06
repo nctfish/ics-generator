c65048ce04a2394c295975df0bb72ebc91c9be98
nctfish <tina.nc.fish@gmail.com>
2016-11-06 15:47:37 +0000

    Applied fixes from StyleCI

--- tests/IcsGeneratorTest.php
+++ tests/IcsGeneratorTest.php
@@ -8,30 +8,30 @@ class IcsGeneratorTest extends \PHPUnit_Framework_TestCase
 {
     public function test_get_ics_file_contents_returns_string_with_values()
     {
-      $icsGenerator = new IcsGenerator();
+        $icsGenerator = new IcsGenerator();
 
-      $testDate = new DateTime;
+        $testDate = new DateTime();
 
-      $fileContents = $icsGenerator
+        $fileContents = $icsGenerator
       ->setDescription('Test Description')
       ->setLocation('Test Location')
       ->setDtStart($testDate)
       ->setDtEnd($testDate)
       ->getString();
 
-      $this->assertInternalType('string', $fileContents);
+        $this->assertInternalType('string', $fileContents);
     }
 
     public function test_setters_are_fluent()
     {
-      $setterMethods = preg_grep('/^set(?!Dt)/', get_class_methods(new IcsGenerator));
+        $setterMethods = preg_grep('/^set(?!Dt)/', get_class_methods(new IcsGenerator()));
 
-      foreach($setterMethods as $setterMethod){
-          $icsGenerator = new IcsGenerator;
+        foreach ($setterMethods as $setterMethod) {
+            $icsGenerator = new IcsGenerator();
 
-          $returnedObject = $icsGenerator->$setterMethod('Hello');
+            $returnedObject = $icsGenerator->$setterMethod('Hello');
 
-          $this->assertInstanceOf(IcsGenerator::class, $returnedObject);
-      }
+            $this->assertInstanceOf(IcsGenerator::class, $returnedObject);
+        }
     }
 }
